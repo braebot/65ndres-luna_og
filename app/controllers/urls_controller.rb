46e1e3d4884fdfa6e9c45e49630936e6d783eada
Andres R <afre92@gmail.com>
2024-11-16 13:28:42 -0500

    form and styles added

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,20 +1,34 @@
 class UrlsController < ApplicationController
   def index
     @url = Url.new
+
+    @urls = Url.all
   end
 
   def create
     puts "Hope fully thi sis the Things #{params}"
+    url = Url.new(url_params)
+    if url.save
+      puts "yay it was saved"
+    else
+    end
   end
 
 
   def fetch_meta_tags(url)
     begin
-      document = Nokogiri::HTML(URI.open(url))
+      document  = Nokogiri::HTML(URI.open(url))
       meta_tags = document.css("meta[property*='og']")
     rescue => e
       puts "An error occurred: #{e.message}"
     end
+  end
+
+
+  private
+
+  def url_params
+    params.require("url").permit("text")
   end
 
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ApplicationRecord
+
+  after_create :enque_job
+
+
+  def enque_job
+  end
 end
